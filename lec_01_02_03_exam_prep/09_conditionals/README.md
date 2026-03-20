79bd7137d924ba9a77e6dc8f135062b638efe622
Samuel Abera <sami21.good.bad@gmail.com>
2026-03-20 09:43:18 +0300

    g

---
+++ lec_01_02_03_exam_prep/09_conditionals/README.md
@@ -0,0 +1,3 @@
+# Step 09 - Conditional Statements
+Slides: 52-55
+Concepts: if, if-else, elseif, nested if, switch.
---
+++ lec_01_02_03_exam_prep/09_conditionals/practice.php
@@ -0,0 +1,5 @@
+<?php
+$score = 75;
+if ($score >= 90) echo "A\n"; elseif ($score >= 75) echo "B\n"; elseif ($score >= 50) echo "C\n"; else echo "F\n";
+$day = "Tuesday";
+switch ($day) { case "Monday": echo "Start\n"; break; case "Tuesday": echo "Second day\n"; break; default: echo "Another day\n"; }
