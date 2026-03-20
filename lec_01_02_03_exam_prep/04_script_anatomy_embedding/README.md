f1b7a2d6c815b3c2d959e36b406366aa06bb211b
Samuel Abera <sami21.good.bad@gmail.com>
2026-03-20 09:41:51 +0300

    g

---
+++ lec_01_02_03_exam_prep/04_script_anatomy_embedding/README.md
@@ -0,0 +1,3 @@
+# Step 04 - Script Anatomy and Embedding
+Slides: 18-21
+Concepts: php tags, semicolons, embed php in html.
---
+++ lec_01_02_03_exam_prep/04_script_anatomy_embedding/practice.php
@@ -0,0 +1,4 @@
+<?php
+$title = "Hello World";
+$message = "It is a perfect day";
+echo "<html><head><title>$title</title></head><body><h1>$title Example</h1><p>$message</p></body></html>";
