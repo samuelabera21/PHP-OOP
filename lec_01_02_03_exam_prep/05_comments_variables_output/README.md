8a2e248a9b08e383efefd5e16a9439abfdf01a48
Samuel Abera <sami21.good.bad@gmail.com>
2026-03-20 09:42:13 +0300

    g

---
+++ lec_01_02_03_exam_prep/05_comments_variables_output/README.md
@@ -0,0 +1,3 @@
+# Step 05 - Comments, Variables, Output
+Slides: 22-28
+Concepts: comments, variable rules, echo/print/print_r/var_dump/var_export.
---
+++ lec_01_02_03_exam_prep/05_comments_variables_output/practice.php
@@ -0,0 +1,3 @@
+<?php
+$name = "Aster"; $age = 20; $skills = ["PHP", "HTML", "MySQL"];
+echo "Name: $name\n"; print "Age: $age\n"; print_r($skills); echo "\n"; var_dump($skills); echo "\n"; var_export($skills); echo "\n";
