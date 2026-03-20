79d75f3c10d7a04581120d6ae3b421bce3386b46
Samuel Abera <sami21.good.bad@gmail.com>
2026-03-20 09:44:39 +0300

    g

---
+++ lec_01_02_03_exam_prep/13_functions_basics/README.md
@@ -0,0 +1,3 @@
+# Step 13 - Functions Basics
+Slides: 72-74
+Concepts: built-in and user-defined functions.
---
+++ lec_01_02_03_exam_prep/13_functions_basics/practice.php
@@ -0,0 +1,4 @@
+<?php
+function writeMessage(){ echo "You are ready to learn functions.\n"; }
+echo date("D") . "\n";
+writeMessage();
